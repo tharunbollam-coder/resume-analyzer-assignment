7d7e396d1d5d1f8becd04fabb50fee75afa78353
tharun <tharunbollam66@gmail.com>
2025-09-07 15:43:59 +0530

    solved cors error

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 if (process.env.NODE_ENV === 'production') {
-  app.use(cors({ origin: 'https://resume-analyzer-assignment-sigma.vercel.app/' }));
+  app.use(cors({ origin: 'https://resume-analyzer-assignment-sigma.vercel.app' }));
 } else {
   app.use(cors()); // open in dev
 }
